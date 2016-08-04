c6be4dfffb2ca065599e7dbbbcf57c7ce7059101
nevada_scout <travis@impactmod.com>
2016-08-04 20:53:21 +0100

    Take params and run command on file change

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,38 @@ extern crate notify;
 use notify::{RecommendedWatcher, Watcher};
 use std::sync::mpsc::channel;
 use std::env;
-use std::string::String;
+use std::path::Path;
+use std::process::Command;
 
 fn watch() -> notify::Result<()> {
 
-    // TODO -- Take two args:
+    // Take two args:
     // 1. file/folder to watch
     // 2. bash script to execute
 
-    // TODO -- Display usage message if num args != 2
+    // Display usage message if num args != 3
+    if env::args().count() != 3 {
+        println!("Invalid usage. Example usage: ./fswatch /path/to/watch /command/to/run.sh");
+        std::process::exit(1);
+    }
 
+    let args: Vec<String> = env::args().collect();
 
+    // Path to watch
+    let ref path = args[1];
+    // Check the supplied path exists
+    if !Path::new(path).exists() {
+        println!("The supplied path '{}' does not exist!", path);
+        std::process::exit(0);
+    }
 
-    let args: Vec<String> = env::args().map(|s| s.into_string().unwrap()).collect();
-
-    // if args.len() > 2 {
-    //     if args[1] == "-c" {
-    //         // Check if args[2] is a valid file path
-    //     } else {
-    //         println!("Please use ./mover -c /path/to/config.json");
-    //     }
-    // }
-
-    // Hardcode config file location temporarily
-    // "~/mover_config.json"
+    // Get the command to run
+    let ref command = args[2];
+    // Check the supplied command exists and is a bash file
+    if !Path::new(command).exists() {
+        println!("The supplied bash command file '{}' does not exist!", path);
+        std::process::exit(0);
+    }
 
     // Create a channel to recieve the events
     let (tx, rx) = channel();
@@ -36,7 +44,8 @@ fn watch() -> notify::Result<()> {
 
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
-    try!(watcher.watch("/Users/joe/dev/mover/test"));
+    try!(watcher.watch(path));
+    println!("Starting file watcher on path '{}'...", path);
 
     // This is a simple loop, but you may want to use more complex logic here,
     // for example to handle I/O.
@@ -44,10 +53,11 @@ fn watch() -> notify::Result<()> {
         match rx.recv() {
             Ok(notify::Event { path: Some(path), op: Ok(op) }) => {
                 println!("{:?} {:?}", op, path);
-                // TODO -- run command from arg 2
                 // TODO -- don't run if something last file change was within 250ms
 
                 // TODO -- wait 100ms before running to check for another change
+
+                Command::new("sh").arg(command).spawn().expect("command failed!");
             }
             Err(e) => println!("watch error {}", e),
             _ => (),
